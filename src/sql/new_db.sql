87bd570acda0d8dbc9910fa79c7384b5bc0b0962
Tobias Brown-Heft <tbrownhe@gmail.com>
2024-11-11 00:17:12 -0800

    Optimize database tables

--- src/sql/new_db.sql
+++ src/sql/new_db.sql
@@ -1,12 +1,19 @@
 --SQLite
+CREATE TABLE "AccountTypes" (
+	"AccountTypeID"	INTEGER NOT NULL UNIQUE,
+	"AccountType"	TEXT UNIQUE,
+	"AssetType"	TEXT,
+	PRIMARY KEY("AccountTypeID" AUTOINCREMENT)
+);
+
 CREATE TABLE "Accounts" (
 	"AccountID"	INTEGER UNIQUE,
-	"NickName"	TEXT UNIQUE,
+	"AccountTypeID"	INTEGER,
 	"Company"	TEXT,
-	"Subtype"	TEXT,
-	"Type"	TEXT,
-	"AssetType"	TEXT,
-	PRIMARY KEY("AccountID" AUTOINCREMENT)
+	"Description"	TEXT,
+	"NickName"	TEXT UNIQUE,
+	PRIMARY KEY("AccountID" AUTOINCREMENT),
+	FOREIGN KEY("AccountTypeID") REFERENCES "AccountTypes"("AccountTypeID")
 );
 
 CREATE TABLE "AccountNumbers" (
@@ -17,41 +24,35 @@ CREATE TABLE "AccountNumbers" (
 	FOREIGN KEY("AccountID") REFERENCES "Accounts"("AccountID")
 );
 
-CREATE TABLE "Cards" (
-	"CardID"	INTEGER UNIQUE,
-	"AccountID"	INTEGER,
-	"CardNumber"	TEXT,
-	"LastFour"	TEXT UNIQUE,
-	PRIMARY KEY("CardID" AUTOINCREMENT),
-	FOREIGN KEY("AccountID") REFERENCES "Accounts"("AccountID")
-);
-
 CREATE TABLE "StatementTypes" (
-	"STID"	INTEGER UNIQUE,
+	"StatementTypeID"	INTEGER UNIQUE,
+	"AccountTypeID"	INTEGER,
 	"Company"	TEXT,
-	"Type"	TEXT,
+	"Description"	TEXT,
 	"Extension"	TEXT,
 	"SearchString"	TEXT,
 	"Parser"	INTEGER,
-	PRIMARY KEY("STID" AUTOINCREMENT)
+	PRIMARY KEY("StatementTypeID" AUTOINCREMENT),
+	FOREIGN KEY("AccountTypeID") REFERENCES "AccountTypes"("AccountTypeID")
 );
 
 CREATE TABLE "Statements" (
 	"StatementID"	INTEGER UNIQUE,
-	"STID"	INTEGER,
-	"MainAccountID"	INTEGER,
+	"StatementTypeID"	INTEGER,
+	"AccountID"	INTEGER,
 	"StartDate"	TEXT,
 	"EndDate"	TEXT,
 	"ImportDate"	TEXT,
 	"Filename"	TEXT,
 	"MD5"	TEXT UNIQUE,
 	PRIMARY KEY("StatementID" AUTOINCREMENT),
-	FOREIGN KEY("MainAccountID") REFERENCES "Accounts"("AccountID"),
-	FOREIGN KEY("STID") REFERENCES "StatementTypes"("STID")
+	FOREIGN KEY("StatementTypeID") REFERENCES "StatementTypes"("StatementTypeID"),
+	FOREIGN KEY("AccountID") REFERENCES "Accounts"("AccountID")
+	
 );
 
 CREATE TABLE "Transactions" (
-	"TranID"	INTEGER,
+	"TransactionID"	INTEGER,
 	"AccountID"	INTEGER,
 	"StatementID"	INTEGER,
 	"Date"	TEXT,
@@ -61,9 +62,18 @@ CREATE TABLE "Transactions" (
 	"MD5"	TEXT UNIQUE,
 	"Category"	TEXT DEFAULT 'Uncategorized',
 	"Verified"	INTEGER DEFAULT 0,
-	PRIMARY KEY("TranID" AUTOINCREMENT),
+	PRIMARY KEY("TransactionID" AUTOINCREMENT),
 	FOREIGN KEY("AccountID") REFERENCES "Accounts"("AccountID"),
 	FOREIGN KEY("StatementID") REFERENCES "Statements"("StatementID")
+);
+
+CREATE TABLE "CardNumbers" (
+	"CardID"	INTEGER UNIQUE,
+	"AccountID"	INTEGER,
+	"CardNumber"	TEXT,
+	"LastFour"	TEXT UNIQUE,
+	PRIMARY KEY("CardID" AUTOINCREMENT),
+	FOREIGN KEY("AccountID") REFERENCES "Accounts"("AccountID")
 );
 
 CREATE TABLE "Shopping" (
@@ -78,30 +88,41 @@ CREATE TABLE "Shopping" (
 	"MD5"	TEXT UNIQUE,
 	PRIMARY KEY("ItemID" AUTOINCREMENT),
 	FOREIGN KEY("AccountID") REFERENCES "Accounts"("AccountID"),
-	FOREIGN KEY("CardID") REFERENCES "Cards"("CardID"),
+	FOREIGN KEY("CardID") REFERENCES "CardNumbers"("CardID"),
 	FOREIGN KEY("StatementID") REFERENCES "Statements"("StatementID")
 );
 
+-- Prepopulate AccountTypes
+INSERT INTO "AccountTypes" ("AccountType", "AssetType")
+VALUES
+	('Checking', 'Asset'),
+	('Savings', 'Asset'),
+	('Credit Card', 'Debt'),
+	('401k', 'Asset'),
+	('HSA', 'Asset'),
+	('Loan', 'Debt'),
+	('Shopping', 'Spending');
+
 -- Prepopulate StatementTypes with statement recognition values
-INSERT INTO "StatementTypes" ("Company", "Type", "Extension", "SearchString", "Parser") 
+INSERT INTO "StatementTypes" ("Company", "AccountTypeID", "Description", "Extension", "SearchString", "Parser") 
 VALUES 
-	('Oregon Community Credit Union', 'Personal Banking', '.pdf', 'oregon community credit union&&member number', 'occubank'),
-	('Oregon Community Credit Union', 'Credit Card', '.pdf', 'www.myoccu.org&&card services', 'occucc'),
-	('Oregon Community Credit Union', 'Auto Loan', '.csv', '254779', 'occuauto'),
-	('Wells Fargo', 'Personal Banking', '.pdf', 'wells fargo everyday checking', 'wfper'),
-	('Wells Fargo', 'Personal Banking', '.pdf', 'wells fargo&&way2save&&savings', 'wfper'),
-	('Wells Fargo', 'Business Banking', '.pdf', 'wells fargo&&initiate business checking', 'wfbus'),
-	('Wells Fargo', 'Business Banking', '.pdf', 'wells fargo&&business market rate savings', 'wfbus'),
-	('Wells Fargo', 'Personal Loan', '.pdf', 'wells fargo&&personal loan statement', 'wfploan'),
-	('Citibank', 'Credit Card', '.pdf', 'www.citicards.com', 'citi'),
-	('US Bank', 'Credit Card', '.pdf', 'u.s. bank&&reivisa.com', 'usbank'),
-	('Fidelity', '401k', '.pdf', 'intel 401(k)&&fidelity', 'fidelity401k'),
-	('Fidelity', 'HSA', '.pdf', 'fidelity health savings account', 'fidelityhsa'),
-	('Health Equity', 'HSA', '.pdf', 'healthequity&&health savings account', 'hehsa'),
-	('Transamerica', '401k', '.pdf', 'transamerica&&retirement account statement', 'transamerica'),
-	('Fedloan Servicing', 'Student Loan', '.xlsx', 'fedloan servicing', 'fedloan'),
-	('Amazon', 'Shopping', '.csv', 'amazon&&unspsc code&&asin/isbn', 'amazonper'),
-	('Amazon', 'Business', '.csv', 'amazon&&account group&&po number', 'amazonbus'),
-	('Vanguard', '401k', '.pdf', 'vanguard.com&&account summary', 'vanguard');
+	('Oregon Community Credit Union', '1', 'Personal', '.pdf', 'oregon community credit union&&member number', 'occubank'),
+	('Oregon Community Credit Union', '3', 'OCCU', '.pdf', 'www.myoccu.org&&card services', 'occucc'),
+	('Oregon Community Credit Union', '6', 'Auto', '.csv', '254779', 'occuauto'),
+	('Wells Fargo', '1', 'Personal', '.pdf', 'wells fargo everyday checking', 'wfper'),
+	('Wells Fargo', '2', 'Personal', '.pdf', 'wells fargo&&way2save&&savings', 'wfper'),
+	('Wells Fargo', '1', 'Business', '.pdf', 'wells fargo&&initiate business checking', 'wfbus'),
+	('Wells Fargo', '2', 'Business', '.pdf', 'wells fargo&&business market rate savings', 'wfbus'),
+	('Wells Fargo', '6', 'Personal', '.pdf', 'wells fargo&&personal loan statement', 'wfploan'),
+	('Citibank', '3', '', '.pdf', 'www.citicards.com', 'citi'),
+	('US Bank', '3', '', '.pdf', 'u.s. bank&&reivisa.com', 'usbank'),
+	('Fidelity', '4', 'Intel', '.pdf', 'intel 401(k)&&fidelity', 'fidelity401k'),
+	('Fidelity', '5', '', '.pdf', 'fidelity health savings account', 'fidelityhsa'),
+	('Health Equity', '5', '', '.pdf', 'healthequity&&health savings account', 'hehsa'),
+	('Transamerica', '4', '', '.pdf', 'transamerica&&retirement account statement', 'transamerica'),
+	('Fedloan Servicing', '6', 'Student', '.xlsx', 'fedloan servicing', 'fedloan'),
+	('Amazon', '7', 'Personal', '.csv', 'amazon&&unspsc code&&asin/isbn', 'amazonper'),
+	('Amazon', '7', 'Business', '.csv', 'amazon&&account group&&po number', 'amazonbus'),
+	('Vanguard', '4', '', '.pdf', 'vanguard.com&&account summary', 'vanguard');
 
 
