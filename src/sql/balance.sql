dcf5310ac3d2334e6d20e1b526173f8ef7282500
Tobias Brown-Heft <tbrownhe@gmail.com>
2024-12-05 00:49:20 -0800

    sql files no longer necessary with SQLalchemy

--- src/sql/balance.sql
+++
@@ -1,8 +0,0 @@
---SQLite
-SELECT
-	Accounts.AccountName,
-	Transactions.Date,
-	Transactions.Balance
-FROM Transactions
-JOIN Accounts ON Transactions.AccountID = Accounts.AccountID
-ORDER BY Transactions.Date ASC, Transactions.TransactionID ASC
--- src/sql/statements.sql
+++
@@ -1,9 +0,0 @@
---SQLite
-SELECT
-	Accounts.AccountName,
-	Statements.StartDate,
-	Statements.EndDate
-FROM Statements
-JOIN Accounts ON Statements.AccountID = Accounts.AccountID
-{where}
-ORDER BY Accounts.AccountName ASC, Statements.StartDate ASC
