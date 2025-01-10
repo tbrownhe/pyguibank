ff420cff16a06785aa48d439042bbaef5903798c
Tobias Brown-Heft <tbrownhe@gmail.com>
2025-01-09 23:09:02 -0800

    rename initialize

--- src/core/initialize.py
+++ src/core/initialize.py
@@ -2,32 +2,29 @@ import shutil
 import sys
 from pathlib import Path
 from loguru import logger
-from core.plugins import PLUGIN_DIR
+from core.settings import settings
 
 
 def initialize_plugins():
     """
     Copies bundled plugins to the user plugin directory if they don't exist.
-    PLUGIN_DIR is defined relative to CONFIG_PATH, which is platform-dependent.
     """
-    # Get the location of the bundled plugins
     plugin_bundled_dir = (
         Path("plugins") if hasattr(sys, "_MEIPASS") else Path("dist/plugins")
     ).resolve()
 
-    # Copy the bundled plugins to the user's PLUGIN_DIR
     try:
-        if not PLUGIN_DIR.exists():
+        if not settings.plugin_dir.exists():
             logger.debug(f"Plugins directory does not exist yet.")
             if not plugin_bundled_dir.exists():
-                PLUGIN_DIR.mkdir(parents=True, exist_ok=True)
+                settings.plugin_dir.mkdir(parents=True, exist_ok=True)
                 logger.debug(
                     f"Bundled plugins not found in {plugin_bundled_dir}, skipping"
                 )
             else:
-                shutil.copytree(plugin_bundled_dir, PLUGIN_DIR)
+                shutil.copytree(plugin_bundled_dir, settings.plugin_dir)
                 logger.debug(
-                    f"Copied bundled plugins from {plugin_bundled_dir} to {PLUGIN_DIR}"
+                    f"Copied bundled plugins from {plugin_bundled_dir} to {settings.plugin_dir}"
                 )
     except Exception as e:
         logger.error(f"Failed to initialize plugins: {e}")
