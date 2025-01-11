25f0a0f49ec3d4e64ba1b359b0a223b4bfe16d9d
Tobias Brown-Heft <tbrownhe@gmail.com>
2025-01-11 11:53:31 -0800

    Plugins named by internal metadata only

--- src/build_plugins.py
+++ src/build_plugins.py
@@ -1,6 +1,7 @@
 import json
 import py_compile
 import shutil
+import sys
 from pathlib import Path
 
 from loguru import logger
@@ -10,19 +11,24 @@ from core.plugins import load_plugin
 # Define source and destination directories
 SOURCE_DIR = Path("src/plugins")
 DEST_DIR = Path("dist/plugins")
-SERVER_DATA_DIR = Path("../pyguibank-server/data/plugins")
+SERVER_DATA_DIR = Path("../pyguibank-server/data/plugins").resolve()
 
 
 def compile_plugins():
     DEST_DIR.mkdir(parents=True, exist_ok=True)
+    SERVER_DATA_DIR.mkdir(parents=True, exist_ok=True)
     for plugin_file in SOURCE_DIR.glob("*.py"):
         if plugin_file.stem == "__init__":
             continue
         try:
             # Load metadata from the plugin
             _, _, metadata = load_plugin(plugin_file)
+
+            # Plugin name based on internal metadata to ensure single source of truth
+            plugin_name = metadata["PLUGIN_NAME"]
             version = metadata["VERSION"]
-            compiled_name = f"{plugin_file.stem}_v{version}.pyc"
+            py_version = f"{sys.version_info.major}{sys.version_info.minor}"
+            compiled_name = f"{plugin_name}_v{version}_p{py_version}.pyc"
             compiled_path = DEST_DIR / compiled_name
 
             # Compile the plugin to a .pyc file
@@ -52,6 +58,9 @@ def generate_metadata():
             if "SEARCH_STRING" in metadata:
                 del metadata["SEARCH_STRING"]
 
+            # Add the filename
+            metadata["FILENAME"] = plugin_file.name
+
             # Add to the list
             metadata_list.append(metadata)
         except Exception as e:
@@ -59,6 +68,8 @@ def generate_metadata():
 
     with metadata_file.open("w") as f:
         json.dump(metadata_list, f, indent=2)
+
+    logger.success(f"Created server metadata file {metadata_file}")
 
 
 if __name__ == "__main__":
